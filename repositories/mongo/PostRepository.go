007926d05627b57b7779af01329231f03b1c1708
samueltoh <samueltoh93@gmail.com>
2021-12-31 11:07:54 +0800

    [POSTS] Sort posts by date created

--- repositories/mongo/PostRepository.go
+++ repositories/mongo/PostRepository.go
@@ -1,9 +1,9 @@
 package mongo
 
 import (
-	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang-clean-arch/configurations"
 	"golang-clean-arch/entities"
 	"golang-clean-arch/exceptions"
@@ -24,7 +24,10 @@ func (p postRepository) GetAll() (response []entities.Post) {
 	ctx, cancel := configurations.NewMongoContext()
 	defer cancel()
 
-	cursor, err := p.Collection.Find(ctx, bson.M{})
+	findOptions := options.Find()
+	findOptions.SetSort(bson.D{{"date_created", -1}})
+
+	cursor, err := p.Collection.Find(ctx, bson.M{}, findOptions)
 	exceptions.PanicIfNeeded(err)
 
 	for cursor.Next(ctx) {
@@ -41,9 +44,6 @@ func (p postRepository) GetAll() (response []entities.Post) {
 func (p postRepository) Create(post entities.Post) {
 	ctx, cancel := configurations.NewMongoContext()
 	defer cancel()
-
-	fmt.Println("repo")
-	fmt.Println(post)
 
 	_, err := p.Collection.InsertOne(ctx, post)
 
